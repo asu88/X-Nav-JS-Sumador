8a7f645723df1672c4af32a607b3f5486508c37f
asu88 <asu.ramon88@gmail.com>
2015-01-26 16:18:38 +0100

    ultimos cambios

--- entrega.js
+++ entrega.js
@@ -1,7 +1,20 @@
 
 
 
-var operandos = ('', '#op').val()
-  = parseInt(); 
+//var operandos = documents.Element('+', '#op').val()
+  //= parseInt();
   
-  //'input[name=n_players]:checked'
+ // var operandos =  document.getElementById("op");
+ // console.log(operandos);  
+  
+  function sumar (id) {
+    var element = document.getElementById(id);
+    console.log(element.innerHTML);
+    //element.innerHTML = newValue;
+  
+  }
+  
+  sumar("op");
+  //n_players= parseInt($('input[name=n_players]:checked', '#game_features').val()); 
+  
+  // http://jsfiddle.net ----para editar y ejecutar online javascript
