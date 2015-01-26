b6ee8aab4899ac9dc18687f6d4867fb1d64d7a99
asu88 <asu.ramon88@gmail.com>
2015-01-26 17:12:14 +0100

    sumador simple version 1.0

--- entrega.js
+++ entrega.js
@@ -1,20 +1,16 @@
 
-
-
-//var operandos = documents.Element('+', '#op').val()
-  //= parseInt();
-  
- // var operandos =  document.getElementById("op");
- // console.log(operandos);  
-  
   function sumar (id) {
     var element = document.getElementById(id);
-    console.log(element.innerHTML);
-    //element.innerHTML = newValue;
+    
+    var string = element.innerHTML;
+       console.log("array " + string.split("+"));
+    var operador = string.split("+");
+    var resultado = parseInt(operador[0]) + parseInt (operador[1]);
+       console.log(resultado);
+    element.innerHTML = resultado;
   
   }
   
   sumar("op");
-  //n_players= parseInt($('input[name=n_players]:checked', '#game_features').val()); 
   
   // http://jsfiddle.net ----para editar y ejecutar online javascript
